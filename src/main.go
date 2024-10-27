466498db33c90bbfb06168000e5060e162860fdc
Nikita Bersenev <bersenev@intervolga.ru>
2024-10-28 02:19:52 +0300

    [develop]

---
+++ src/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"log"
+)
+
+func homeHandler(c fiber.Ctx) error {
+	return c.SendString("homeHandler")
+}
+
+func projects(c fiber.Ctx) error {
+	return c.SendString("projects")
+}
+
+func project(c fiber.Ctx) error {
+	if c.Params("name") == "" {
+		return c.SendString("project")
+	}
+
+	return c.SendString("project " + c.Params("name"))
+}
+
+func articles(c fiber.Ctx) error {
+	return c.SendString("articles")
+}
+
+func article(c fiber.Ctx) error {
+	if c.Params("name") == "" {
+		return c.SendString("article")
+	}
+
+	return c.SendString("article " + c.Params("name"))
+}
+
+func contacts(c fiber.Ctx) error {
+	return c.SendString("contacts")
+}
+
+func main() {
+	app := fiber.New()
+	app.Get("/", homeHandler)
+	app.Get("/projects", projects)
+	app.Get("/project/:name", project)
+	app.Get("/articles", articles)
+	app.Get("/article/:name", article)
+	app.Get("/contacts", contacts)
+
+	log.Fatal(app.Listen(":8080"))
+}
